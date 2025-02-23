1ec3201fb4d9c1c8e14ec5212640346a09158b7f
DBbluu <skyhuntersht@gmail.com>
2025-02-23 20:58:10 +0700

    Water

--- App/app/(tabs)/Report.jsx
+++ App/app/(tabs)/Report.jsx
@@ -1,9 +1,9 @@
 import { React, useState, useEffect } from "react";
-import { Text, View, StyleSheet,ScrollView, ActivityIndicator } from "react-native";
+import { Text, View, StyleSheet, ScrollView, ActivityIndicator, TouchableOpacity } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Calendar } from "react-native-calendars";
 import { auth, db } from '../../src/firebaseConfig'; // Import Firebase
-import { doc, getDoc,getFirestore } from 'firebase/firestore'; // Firestore functions
+import { doc, getDoc, getFirestore, setDoc, updateDoc } from 'firebase/firestore'; // Firestore functions
 import { ProgressBar } from 'react-native-paper';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useTheme } from '../../context/ThemeContext'; // Import useTheme
@@ -79,6 +79,7 @@ export default function ReportScreen() {
           }}
         />
         <BMIComponent/>
+        <WaterIntakeComponent/>
         </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -117,6 +118,7 @@ const Stats = ({ workOut, Kcal, Time }) => {
     </SafeAreaView>
   );
 };
+
 const BMIComponent = () => {
   const [weight, setWeight] = useState("-");
   const [height, setHeight] = useState("-");
@@ -178,12 +180,134 @@ const BMIComponent = () => {
     progress = 1;
     color = '#F44336';
   }
+
+  const increaseWeight = () => setWeight((prevWeight) => (parseFloat(prevWeight) + 1).toString());
+  const decreaseWeight = () => setWeight((prevWeight) => (parseFloat(prevWeight) - 1).toString());
+
   return (
     <View style={styles.container1}>
       <Text style={styles.title}>BMI (kg/m²)</Text>
       <Text style={styles.bmi}>{bmi}</Text>
       <Text style={[styles.category, { color }]}>{category}</Text>
       <ProgressBar progress={progress} color={color} style={styles.progressBar} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.smallerButton} onPress={decreaseWeight}>
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.smallerButton} onPress={increaseWeight}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const WaterIntakeComponent = () => {
+  const [waterIntake, setWaterIntake] = useState(0);
+  const [totalWaterIntake, setTotalWaterIntake] = useState(0);
+  const [uid, setUid] = useState(null);
+  const [currentDate, setCurrentDate] = useState(new Date().toISOString().split('T')[0]);
+  const [recommendedWaterIntake, setRecommendedWaterIntake] = useState("");
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUid(user.uid);
+      } else {
+        console.log("No user is signed in.");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (uid) {
+        const db = getFirestore();
+        const docRef = doc(db, "users", uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          const age = data.age;
+          const gender = data.gender;
+          setRecommendedWaterIntake(getRecommendedWaterIntake(age, gender));
+        }
+      }
+    };
+
+    fetchData();
+  }, [uid]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (uid) {
+        const db = getFirestore();
+        const docRef = doc(db, "users", uid, "progress", "waterIntake");
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          if (data.date !== currentDate) {
+            await updateDoc(docRef, { waterIntake: 0, date: currentDate });
+            setTotalWaterIntake(0);
+          } else {
+            setTotalWaterIntake(data.waterIntake || 0);
+          }
+        } else {
+          console.log("No such document!");
+          await setDoc(docRef, { waterIntake: 0, date: currentDate }); // Create the document if it doesn't exist
+        }
+      }
+    };
+
+    fetchData();
+  }, [uid, currentDate]);
+
+  const increaseWaterIntake = () => setWaterIntake((prev) => prev + 1);
+  const decreaseWaterIntake = () => setWaterIntake((prev) => (prev > 0 ? prev - 1 : 0));
+
+  const confirmWaterIntake = async () => {
+    if (uid) {
+      const newTotalWaterIntake = totalWaterIntake + waterIntake;
+      setTotalWaterIntake(newTotalWaterIntake);
+      setWaterIntake(0); // Reset the current water intake
+      const db = getFirestore();
+      const docRef = doc(db, "users", uid, "progress", "waterIntake");
+      await updateDoc(docRef, { waterIntake: newTotalWaterIntake, date: currentDate });
+    }
+  };
+
+  const getRecommendedWaterIntake = (age, gender) => {
+    if (age >= 1 && age <= 3) return "4-5";
+    if (age >= 4 && age <= 8) return "5-7";
+    if (age >= 9 && age <= 12) return "7-10";
+    if (age >= 13 && age <= 18) return gender === "male" ? "10-12" : "8-10";
+    if (age >= 19 && age <= 60) return gender === "male" ? "10-12" : "8-10";
+    if (age > 60) return "6-8";
+    return "";
+  };
+
+  return (
+    <View style={styles.container1}>
+      <Text style={styles.title}>Water Intake</Text>
+      <Text style={styles.waterIntakeText}>
+        <Text style={{ color: '#32CD32' }}>{totalWaterIntake}</Text> / {recommendedWaterIntake}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.smallerButton} onPress={decreaseWaterIntake}>
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+        <Text style={styles.waterIntake}>{waterIntake}</Text>
+        <TouchableOpacity style={styles.smallerButton} onPress={increaseWaterIntake}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.smallerConfirmButton} onPress={confirmWaterIntake}>
+          <Text style={styles.confirmButtonText}>Confirm</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -246,37 +370,101 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     borderRadius: 12,
     elevation: 5,
-    marginTop:20,
-},
-title: {
+    marginTop: 20,
+  },
+  title: {
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
-},
-bmi: {
+  },
+  bmi: {
     fontSize: 32,
     fontWeight: 'bold',
     color: '#333',
-},
-category: {
+  },
+  category: {
     fontSize: 16,
     marginBottom: 10,
-},
-progressBar: {
+  },
+  progressBar: {
     width: '100%',
     height: 10,
     borderRadius: 5,
     marginBottom: 10,
-},
-input: {
-    width: '80%',
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  button: {
+    backgroundColor: '#1f66f2',
+    padding: 10,
     borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 40,
+    height: 40,
+  },
+  smallerButton: {
+    backgroundColor: '#1f66f2',
+    padding: 3,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 25,
+    height: 25,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  waterIntake: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333',
+    marginHorizontal: 10,
+  },
+  confirmButton: {
+    backgroundColor: '#1f66f2',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+  smallerConfirmButton: {
+    backgroundColor: '#1f66f2',
+    padding: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+  confirmButtonText: {
+    color: '#fff',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  totalWaterText: {
+    fontSize: 18,
+    color: '#333',
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+  smallerTotalWaterText: {
+    fontSize: 16,
+    color: '#333',
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+  waterIntakeText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333',
     marginBottom: 10,
-    paddingHorizontal: 10,
-    textAlign: 'center',
-},
+  },
 });
 
